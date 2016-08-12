17a8eb346663d47f23bef34a011bf1b088b1ffe3
ipurvis <ian.c.purvis@gmail.com>
2016-08-12 08:40:14 -0400

    Fixes swift 2.3 build errors

--- ImagePickerSheetController/ImagePickerSheetController/AnimationController.swift
+++ ImagePickerSheetController/ImagePickerSheetController/AnimationController.swift
@@ -23,10 +23,8 @@ class AnimationController: NSObject {
     // MARK: - Animation
     
     private func animatePresentation(context: UIViewControllerContextTransitioning) {
-        guard let containerView = context.containerView() else {
-            return
-        }
-        
+        let containerView = context.containerView()
+
         containerView.addSubview(imagePickerSheetController.view)
         
         let sheetOriginY = imagePickerSheetController.sheetCollectionView.frame.origin.y
@@ -42,9 +40,7 @@ class AnimationController: NSObject {
     }
     
     private func animateDismissal(context: UIViewControllerContextTransitioning) {
-        guard let containerView = context.containerView() else {
-            return
-        }
+        let containerView = context.containerView()
         
         UIView.animateWithDuration(transitionDuration(context), delay: 0, options: .CurveEaseIn, animations: { () -> Void in
             self.imagePickerSheetController.sheetCollectionView.frame.origin.y = containerView.bounds.maxY
